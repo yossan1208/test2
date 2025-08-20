edae3ecc6280b0e1ff85d0b47cdf4f29efa319f6
Yuki Yoshizawa <68634838+yossan1208@users.noreply.github.com>
2025-08-20 23:37:21 +0900

    Javaファイルの追加

---
+++ TestProject01/src/java03/Java03.java
@@ -0,0 +1,8 @@
+package java03;
+
+public class Java03 {
+	public static void main(String[] args) {
+		System.out.println("こんにちは！！");
+	}
+}
+
---
+++ TestProject01/src/java03/java3_2.java
@@ -0,0 +1,7 @@
+package java03;
+
+public class java3_2 {
+	public static void main(String[] args) {
+		System.out.println("例外が\r\n発生しました。");
+	}
+}
