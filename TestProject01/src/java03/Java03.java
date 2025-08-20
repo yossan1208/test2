2a416f428d58109986f3092fe2e8621d7ac8a8b6
Yuki Yoshizawa <68634838+yossan1208@users.noreply.github.com>
2025-08-21 00:22:09 +0900

    Delete TestProject01 directory

--- TestProject01/src/java03/Java03.java
+++
@@ -1,8 +0,0 @@
-package java03;
-
-public class Java03 {
-	public static void main(String[] args) {
-		System.out.println("こんにちは！！");
-	}
-}
-
--- TestProject01/src/java03/java3_2.java
+++
@@ -1,7 +0,0 @@
-package java03;
-
-public class java3_2 {
-	public static void main(String[] args) {
-		System.out.println("例外が\r\n発生しました。");
-	}
-}
